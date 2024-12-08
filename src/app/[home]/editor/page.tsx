822088eef1a8cb190ef3b926aafb0274c5057461
AREEBA-FATIMA <areebafatima2457@gmail.com>
2024-12-08 20:34:16 +0500

    hackathon

--- src/app/[home]/editor/page.tsx
+++ src/app/[home]/editor/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="min-h-[100vh] flex flex-col items-center bg-gray-50">
       <header className="text-center py-10">
-        <h1 className="text-2xl font-bold text-gray-800">EDITOR'S PICK</h1>
+        <h1 className="text-2xl font-bold text-gray-800">EDITOR&apos;S PICK</h1>
         <p className="text-sm text-gray-500 mt-2">
           Problems trying to resolve the conflict between
         </p>
